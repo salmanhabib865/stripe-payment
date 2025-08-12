1794c3e5384d8819635d47ca97536fe332c65374
salmanhabib865 <salmanhabib865@gmail.com>
2025-08-12 19:59:45 +0500

    Update StripePaymentApplication.java
    main class

--- src/main/java/com/tech/stripe_payment/StripePaymentApplication.java
+++ src/main/java/com/tech/stripe_payment/StripePaymentApplication.java
@@ -2,12 +2,19 @@ package com.tech.stripe_payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StripePaymentApplication {
+public class StripePaymentApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StripePaymentApplication.class, args);
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(StripePaymentApplication.class);
 	}
+	 
+	public static void main(String[] args) {
+        SpringApplication.run(StripePaymentApplication.class, args);
+    }
 
 }

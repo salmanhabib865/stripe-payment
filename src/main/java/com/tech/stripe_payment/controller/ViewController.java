52a2dd092c45a8a18a4864c450214fd211eeb9e5
salmanhabib865 <salmanhabib865@gmail.com>
2025-08-12 23:59:35 +0500

    controller

--- src/main/java/com/tech/stripe_payment/controller/ViewController.java
+++ src/main/java/com/tech/stripe_payment/controller/ViewController.java
@@ -1,4 +1,4 @@
-package com.tech.stripe_payment;
+package com.tech.stripe_payment.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
